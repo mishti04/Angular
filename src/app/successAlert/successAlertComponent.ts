e8afbec1d0a2a2a8f6c6a6ed7ec285a02be153e7
shilpa <shilpadolai@gmail.com>
2024-04-11 18:36:09 +0530

    new components with diff types of selectors

--- src/app/successAlert/successAlertComponent.ts
+++ src/app/successAlert/successAlertComponent.ts
@@ -2,11 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: '.success-Alert',
-    template: `<div class =".success-Alert"> SuccessAlert</div>
-    <div class =".success-Alert"> WarningAlert</div>`,
+    template: `<div> SuccessAlert</div>`,
     styles:`div {
-        color: darkblue;
-      }
+      color: green;
+    }  
       `
 })
 

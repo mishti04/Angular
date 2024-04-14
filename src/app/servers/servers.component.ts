4df1bac7b65a512b0e7beb67b13b150d5de24f06
shilpa <shilpadolai@gmail.com>
2024-04-14 13:20:17 +0530

    2 way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,14 +9,17 @@ import { Component, OnInit } from '@angular/core';
 
   //selecting by element
    selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+   }
 
   ngOnInit() {
   }
